cfbbfd979b30703ca00f280187cda940e069ab17
eric <119855695+shih0719@users.noreply.github.com>
2025-03-25 17:58:20 +0800

    config 區分不同檔案

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const mqtt2modbus = require('./lib/core');
-const config = require('./lib/config');
+const mqtt2modbus = require("./lib/core");
 
-const Service = new mqtt2modbus(config);
+const Service = new mqtt2modbus();
 Service.startMQTT();
 Service.startModbus();
-Service.on('mqttConnected', () => {
-    Service.subscribe('test/GW/+');
-    Service.subscribe('eastsoft/0000IPX8/+');
+Service.on("mqttConnected", () => {
+  Service.subscribe("test/GW/+");
+  Service.subscribe("eastsoft/0000IPX8/+");
 });
